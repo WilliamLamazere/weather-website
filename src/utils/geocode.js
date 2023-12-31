c30cc8b2ae3263dbd5bc8f231ecfffc95bdc4104
W Lamazere <william.lamazere@dynatrace.com>
2023-12-31 16:44:19 +0100

    adding env var for mapbox token

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 const request = require('request')
 
+
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoid2lsbHlib2FyZCIsImEiOiJja2VzejRseWcyaXljMnJtcXB0dmw3MnZsIn0.t80WsTDsEYBWx0zFkGlzTQ'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token='+process.env.MAPBOX_TOKEN
     request({ url: url, json: true }, (error, response) => { 
         if (error) {
         callback('Unable to connect to location services!', undefined) } 
